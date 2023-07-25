3a989adcfb7dec551145c5f361d15fbb6af6aded
richard matheus <richard.tech99@gmail.com>
2023-07-25 19:40:24 -0300

    add: component list footer

--- front-end/components/Footer/index.tsx
+++ front-end/components/Footer/index.tsx
@@ -1,32 +1,83 @@
 import React from 'react';
 import Image from 'next/image';
-import { FooterContainer, FooterBox, FooterColumn, CopyrightText, Title } from './styles';
+import Link from 'next/link';
+import ListPages from './components/ListPages/index';
+import { FooterContainer, FooterBox, FooterColumn, CopyrightText, Title, DivIcon, ConatainerIcon, DivTitle } from './styles';
 import { Container } from '../../global';
 
-// const columns = [
-//   {
-//     title: 'Pages',
-//     iconSrc: '/svgs/book.svg',
-//     iconAlt: 'Book',
-//     iconWidth: 29,
-//     iconHeight: 38,
-//   },
-//   {
-//     title: 'Pages',
-//     iconSrc: '/svgs/book.svg',
-//     iconAlt: 'Book',
-//     iconWidth: 29,
-//     iconHeight: 38,
-//   },
-//   {
-//     title: 'Pages',
-//     iconSrc: '/svgs/book.svg',
-//     iconAlt: 'Book',
-//     iconWidth: 29,
-//     iconHeight: 38,
-//   },
-//   // Adicione outras colunas aqui conforme necessário
-// ];
+interface IconPros {
+  title: string;
+  iconSrc: string;
+  iconAlt: string;
+  iconWidth: number;
+  iconHeight: number;
+  link: string;
+}
+
+interface ItemsProps {
+  text: string;
+  link: string;
+}
+
+const icons: IconPros[] = [
+  {
+    title: 'Pages',
+    iconSrc: '/svgs/facebook.svg',
+    iconAlt: 'Facebook',
+    iconWidth: 15,
+    iconHeight: 15,
+    link: 'https://www.facebook.com/'
+  },
+  {
+    title: 'Pages',
+    iconSrc: '/svgs/twitter.svg',
+    iconAlt: 'Twitter',
+    iconWidth: 15,
+    iconHeight: 15,
+    link: 'https://www.twitter.com/'
+  },
+  {
+    title: 'Pages',
+    iconSrc: '/svgs/linkedin.svg',
+    iconAlt: 'Linkedin',
+    iconWidth: 15,
+    iconHeight: 15,
+    link: 'https://www.linkedin.com/'
+  },
+  {
+    title: 'Pages',
+    iconSrc: '/svgs/instagram.svg',
+    iconAlt: 'Instagram',
+    iconWidth: 15,
+    iconHeight: 15,
+    link: 'https://www.instagram.com/'
+  }
+];
+
+
+const items: ItemsProps[] = [
+  { 
+    text: 'Home', 
+    link:'/' 
+  },
+  { 
+    text: 'About Us',
+    link:'/'  
+  },
+  { 
+    text: 'Services',
+    link:'/' 
+  },
+  { 
+    text: 'Blog',
+    link:'/' 
+  },
+  { 
+    text: 'Contact Us',
+    link:'/'  
+  },
+];
+
 
 const Footer: React.FC = () => {
   return (
@@ -34,15 +85,38 @@ const Footer: React.FC = () => {
       <Container>
         <FooterBox>
           <FooterColumn>
-            <Title>
+            <DivTitle>
               <Image
-                src='/svgs/book.svg'
-                alt='Book'
-                width={29}
-                height={38}
-              />
-              Pages
+                  src='/svgs/book.svg'
+                  alt='Book'
+                  width={29}
+                  height={38}
+                  style={{ marginRight: '5px', marginLeft: '5px' }}
+                />
+              <Title>
+                Pages
+              </Title>
+            </DivTitle>
+            <ConatainerIcon>
+              {icons.map((icon, index) => (
+                <Link href={icon.link} passHref key={index} target="_blank">
+                  <DivIcon>
+                    <Image
+                      src={icon.iconSrc}
+                      alt={icon.iconAlt}
+                      width={icon.iconWidth}
+                      height={icon.iconHeight}
+                    />
+                  </DivIcon>
+                </Link>
+              ))}
+            </ConatainerIcon>
+          </FooterColumn>
+          <FooterColumn>
+            <Title>
+              Explore
             </Title>
+            <ListPages items={items} />
           </FooterColumn>
         </FooterBox>
         <CopyrightText>&copy; {new Date().getFullYear()} Bookshelf</CopyrightText>
