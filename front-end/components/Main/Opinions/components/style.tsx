3114483d7208de5edd8fd468a8964aef87fb411a
Gabriel <gabrielsantana2267@gmail.com>
2023-08-01 21:31:42 -0300

    :construction: add: Production about books

---
+++ front-end/components/Main/Opinions/components/style.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export const Container = styled.div`
+    display: flex;
+    width: 20.125rem;
+    height: 18.3125rem;
+    padding: 2.25rem 1.9375rem;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.625rem;
+    background: #1B3764;
+    border-radius: 10px;
+`
+
+export const DivPersons = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+`
+
+export const Name = styled.h3`
+    color: #FFF;
+    font-family: Cardo;
+    font-size: 1.5rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 120%; /* 1.8rem */
+    letter-spacing: -0.03rem;
+    margin: 0;
+`
+export const DivName = styled.div`
+    gap: .3rem;
+    display: flex;
+    flex-direction: column;
+`
+
+export const Title = styled.h3`
+    color: #FFF;
+    font-family: Cardo;
+    font-size: 1.375rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 120%; /* 1.65rem */
+    letter-spacing: -0.01375rem;
+    margin-bottom: 0;
+`
+
+export const Opinion = styled.p`
+    color: #B4C7E7;
+    font-family: Inter;
+    font-size: 1.1875rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 180%; /* 2.1375rem */
+    letter-spacing: -0.01188rem;
+    width: 18rem;
+    text-align: justify;
+`
---
+++ front-end/components/Main/Opinions/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+//Components
+import { Container } from "../AboutAuthorHome/style";
+import { TitleSection } from "../AboutAuthorHome/style";
+import { LineYellow } from "../HeroSection/style";
+
+//StyledComponents
+import { TitleContainer, Description, StarsDiv, NumberStars, Overall, LeftDiv, RigthDiv, Section } from "./style";
+
+//Image
+import stars from '../../../public/stars.svg'
+import Image from "next/image";
+import OpinionsCard from "./components";
+
+const numberStars: string = '4.8'
+const Opinions = () => {
+    return (
+        <Container>
+            <Section>
+                <LeftDiv>
+                    <TitleContainer>
+                        <TitleSection>What Readers Say About the Book</TitleSection>
+                        <LineYellow></LineYellow>
+                    </TitleContainer>
+                    <Description>
+                        If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators.
+                    </Description>
+                    {numberStars === '4.8' && (
+                        <StarsDiv>
+                            <Image
+                                src={stars}
+                                alt="stars"
+                            />
+                            <NumberStars>(4.8/5)</NumberStars>
+                        </StarsDiv>
+                    )}
+                    <Overall>Overall Customer Ratings</Overall>
+                </LeftDiv>
+                <RigthDiv>
+                    <OpinionsCard/>
+                </RigthDiv>
+            </Section>
+        </Container>
+    )
+}
+
+export default Opinions
